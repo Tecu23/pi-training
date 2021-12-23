46e303337b047557d67a73cad2a6e38e5ddfd937
Tecu 23 <andreitecu31@gmail.com>
2021-12-23 12:36:17 -0500

    buzzer with digital inputs - lesson 11

---
+++ lesson_11/buzzer_with_input.py
@@ -0,0 +1,43 @@
+#import the libraries used
+import time
+import pigpio
+
+#create an instance of the pigpio library
+pi = pigpio.pi()
+
+#define the pin used by the Buzzer this pin will be used by the pigpio library which takes the pins in GPIO forms
+#we will use GPIO18, which is pin 12
+buzzer = 18
+
+#set the pin used by the buzzer as OUTPUT
+pi.set_mode(buzzer, pigpio.OUTPUT)
+
+#define the pin used by the button
+#here we will use the GPIO number
+button = 8 #pin 24 in BOARD mode
+
+#set the pins for the buttons as INPUT, and we will set the initial value to On, or we can say that will be pulled up
+pi.set_pull_up_down(button, pigpio.PUD_UP)
+
+try:
+    while True:
+        #check if the button was pressed
+        while pi.read(button) == 0:
+            #turn on the buzzer at a frequency of 500 Hz
+            pi.hardware_PWM(buzzer, 500, 500000)
+
+        #check if the button is released
+        if pi.read(button) == 1:
+            #turn off the buzzer
+            pi.hardware_PWM(buzzer, 0, 0)
+
+        #wait 100 ms before next run
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+
+#stop the PWM signal
+pi.write(buzzer, 0)
+
+#stop the connection with the daemon
+pi.stop()
